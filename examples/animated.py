daca2b7c7d089d0d3c3c3ebd4bde2300c4f3dde0
tcbegley <tom@asidatascience.com>
2019-01-25 09:39:53 +0000

    Add animated example

---
+++ examples/animated.py
@@ -0,0 +1,47 @@
+"""
+Animated example app based on https://react-google-charts.com/animations
+"""
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import numpy as np
+import pandas as pd
+from dash.dependencies import Input, Output
+from dash_google_charts import Chart
+
+app = dash.Dash()
+
+app.layout = html.Div(
+    [
+        dcc.Interval(id="interval", interval=1000),
+        Chart(
+            id="scatter",
+            chartType="ScatterChart",
+            options={
+                "title": "A Scatter Plot",
+                "animation": {
+                    "duration": 1000,
+                    "easing": "out",
+                    "startup": True,
+                },
+                "vAxis": {"viewWindow": {"max": -10, "min": 10}},
+                "hAxis": {"viewWindow": {"max": 10, "min": -10}},
+            },
+        ),
+    ]
+)
+
+
+@app.callback(Output("scatter", "data"), [Input("interval", "n_intervals")])
+def generate_data(n):
+    data = pd.DataFrame(
+        {
+            "x": np.random.uniform(-10, 10, 20),
+            "y": np.random.uniform(-10, 10, 20),
+        }
+    )
+    return data.T.reset_index().T.values.tolist()
+
+
+if __name__ == "__main__":
+    app.run_server(port=8888, debug=True)
