d5368f4e7e05990051b718b340b2ab47295cb7c1
tcbegley <tom@asidatascience.com>
2019-02-02 22:26:11 +0000

    Update examples to use new components

--- examples/chart-types/pie_chart.py
+++ examples/chart-types/pie_chart.py
@@ -1,23 +1,38 @@
 """
 Example app based on https://react-google-charts.com/pie-chart
 """
+import json
+
 import dash
-from dash_google_charts import Chart
+import dash_html_components as html
+from dash.dependencies import Input, Output
+from dash_google_charts import PieChart
 
 app = dash.Dash()
 
-app.layout = Chart(
-    chartType="PieChart",
-    data=[
-        ["Task", "Hours per Day"],
-        ["Work", 11],
-        ["Eat", 2],
-        ["Commute", 2],
-        ["Watch TV", 2],
-        ["Sleep", 7],
-    ],
-    options={"title": "My Daily Activities"},
+app.layout = html.Div(
+    [
+        PieChart(
+            id="chart",
+            data=[
+                ["Task", "Hours per Day"],
+                ["Work", 11],
+                ["Eat", 2],
+                ["Commute", 2],
+                ["Watch TV", 2],
+                ["Sleep", 7],
+            ],
+            options={"title": "My Daily Activities"},
+        ),
+        html.Div(id="output"),
+    ]
 )
+
+
+@app.callback(Output("output", "children"), [Input("chart", "selection")])
+def print_selection(selection):
+    print(selection)
+    return json.dumps(selection)
 
 
 if __name__ == "__main__":
