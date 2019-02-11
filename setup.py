0bbca06cd94b8d0617e2306d76c15a4acd4463c1
tcbegley <tom@asidatascience.com>
2019-02-11 20:46:28 +0000

    Kill Python 2 support

--- setup.py
+++ setup.py
@@ -35,8 +35,6 @@ setup(
     classifiers=[
         "Development Status :: 3 - Alpha",
         "License :: OSI Approved :: Apache Software License",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
     ],
